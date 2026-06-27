886b74df0841915f84d69c43eda1e1494a8d66a7
Hamza Elfathi <elf.hamza2000@gmail.com>
2026-06-27 16:03:16 +0100

    flaky tests

--- tests/customer.spec.ts
+++ tests/customer.spec.ts
@@ -1,31 +1,33 @@
 import { test, expect } from '@playwright/test';
 
 test('add customer', async ({ page }) => {
-  test.setTimeout(15000);
+  test.setTimeout(25000);
+  const uniqueEmail = `hamza.${Date.now()}@sfr.fr`;
+  
   await page.goto('/customers');
   await expect(page.getByTestId('sidebar-customers')).toBeVisible();
   await page.getByTestId('create-customer-button').click();
-  await page.getByLabel('Name').fill('Hamza Elfathi');
-  await page.getByLabel('Email').fill('elf123@sfr.fr');
+  await page.getByLabel('Name').fill('H Elfathi');
+  await page.getByLabel('Email').fill(uniqueEmail );
   await page.getByLabel('Status').selectOption('Active');
+  
   await page.getByRole('button', { name: 'Save customer' }).click();
+  await expect(page.getByRole('button', { name: 'Delete customer' })).toBeVisible({ timeout: 10000 });
   await page.getByTestId('sidebar-customers').click();
-  await expect(page.getByRole('link', { name: 'Hamza Elfathi' })).toBeVisible({ timeout: 10000 });
+  await expect( page.getByRole('link', { name: 'H Elfathi' })).toBeEnabled ;
 });
 
-
 test('delete customer', async ({ page }) => {
-  test.setTimeout(20000);
+  test.setTimeout(25000);
+   const uniqueEmail = `hamza.${Date.now()}@sfr.fr`;
   await page.goto('/customers');
   await expect(page.getByTestId('sidebar-customers')).toBeVisible();
   await page.getByTestId('create-customer-button').click();
   await page.getByLabel('Name').fill('Hamza Elfathi');
-  await page.getByLabel('Email').fill('elf123@sfr.fr');
+  await page.getByLabel('Email').fill(uniqueEmail);
   await page.getByLabel('Status').selectOption('Active');
   await page.getByRole('button', { name: 'Save customer' }).click();
-  await page.waitForURL('**/customers/*');
-  await page.getByRole('button', { name: 'Delete customer' }).click({ timeout: 10000 });
-  await page.waitForURL('**/customers');
+  await page.getByRole('button', { name: 'Delete customer' }).click();
   await expect(page.getByRole('link', { name: 'Hamza Elfathi' })).not.toBeVisible({ timeout: 10000 });
 });
 
