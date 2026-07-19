e505c3ee1259d62e57dff36cdfea807f25be0613
apple <apple@MacBook-Air-de-apple.local>
2026-07-19 19:50:12 +0200

    new features fixed

--- tests/UI/orders.spec.ts
+++ tests/UI/orders.spec.ts
@@ -10,14 +10,15 @@ test('modifier order', async ({ page }) => {
     await page.getByLabel('Product').selectOption('Mac13Pro');
     await page.getByRole('button', { name: 'Add item' }).click();
     await page.getByRole('button', { name: 'Checkout cart' }).click();
-        await page.getByRole('button', { name: 'Ship order' }).click();
+        await page.getByRole('button', { name: 'Ship' }).click();
     await expect(page.getByText('Order shipped.')).toBeVisible();
 });
 
 
 test('cancel order', async ({ page }) => {
-  await page.getByRole('button', { name: 'Checkout cart' }).click();
- // await expect(page.getByRole('button', { name: 'Cancel order' })).toBeEnabled();
-  page.getByRole('button', { name: 'Cancel order' }).click();
+await page.getByRole('button', { name: 'Checkout cart' }).click();
+const cancel = page.getByRole('button', { name: 'Cancel order' })
+ await expect(cancel).toBeVisible();
+ await (cancel).click();
   await expect(page.getByText('Order cancelled.')).toBeVisible();
 });
